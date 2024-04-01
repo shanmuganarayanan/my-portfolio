619db26cf8d19b084c39920329c660c46334c65c
shanmuganarayanan <shanmuga05111999@gmail.com>
2024-04-01 19:52:22 +0530

    content updated

--- src/Pages/Works/Works.js
+++ src/Pages/Works/Works.js
@@ -34,7 +34,7 @@ const Works = ({
                             <Subtitletext>Website & Product Development</Subtitletext>
                             <Subtitletext>March 2023 - Present</Subtitletext>
                         </Subtitlewrapper>
-                        <Descriptiontext>Brainstormed along with the team to build the initial version of the product. Conducted competitive analysis and was responsible for designing the end to end design of the product and website</Descriptiontext>
+                        <Descriptiontext>I am responsible the end to end development of Huddlenow product and website along with another developer. Working closely with the Co-founders and cross functional teams in building the beta version of the application for the customers and investors.</Descriptiontext>
                         <Buttonwrapper>
                             <Buttonprimary onClick={productHandler}>
                                 View Product
